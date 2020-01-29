c8ba0c3d411c806ccf9a48d47abe153c10b29404
Bahadir Arslan <arslan.bah@gmail.com>
2020-01-29 19:32:50 +0100

    Added normal blink since I did not have one.

---
+++ esp8266/blink/blink.ino
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+
+void strobe(){
+    for (uint8_t i = 0; i < 5; i++)
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(25);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(25);
+    }
+    delay(250);
+}
+
+void setup(){
+    pinMode(LED_BUILTIN, OUTPUT);    
+}
+
+void loop(){
+
+    strobe();
+
+}
